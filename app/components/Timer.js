02166a6d27ed191fa439cb614c629ff2cbb06fc4
Captaintobi <tinh572@gmail.com>
2018-10-23 21:35:50 -0700

    added stylesheet

--- app/components/Timer.js
+++ app/components/Timer.js
@@ -1,30 +1,40 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import './main.scss';
 
 
 export default class Timer extends React.Component{
 	
+	// refactor the time so that it is in the format of minutes:seconds
+	refactorTime = (seconds) => {
+		var minutes = Math.floor(seconds / 60)
+		var remainingSeconds = seconds % 60
+
+		return  minutes + ':' + remainingSeconds
+	}
 
 	render(){
 		const { time } = this.props
 		return(
 			<div>
-				<Button variant="outlined"
-					color="secondary" 
-					onClick={this.props.startTimer}>
-					Start Timer
-				</Button>
-				<Button variant="outlined" 
-					color="secondary" 
-					onClick={this.props.resetTimer}>
-					Reset Timer
-				</Button>
-				<Button variant="outlined" 
-					color="secondary" 
-					onClick={this.props.toggleTimer}>
-					Pause/Resume	
-				</Button>
-				<span>{time}</span>
+				<div id="controller">
+					<Button variant="outlined"
+						color="secondary" 
+						onClick={this.props.startTimer}>
+						Start Timer
+					</Button>
+					<Button variant="outlined" 
+						color="secondary" 
+						onClick={this.props.resetTimer}>
+						Reset Timer
+					</Button>
+					<Button variant="outlined" 
+						color="secondary" 
+						onClick={this.props.toggleTimer}>
+						Pause/Resume	
+					</Button>
+					<span className='timer' >{this.refactorTime(time)}</span>
+				</div>
 			</div>
 
 		)
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -20,76 +20,166 @@ export default class HomePage extends React.PureComponent {
 	// There should be a table to track which minutes the user is going to have
 	// TODO have the user pick out which times that he/she wants to have
 	// TODO maybe I can have the user pick out how many section he/she wants
-	// TODO Make it a way so that i can stop the timer PAUSE TIMER
-	//I can always disable the button if intervalRunning is true
+	// TODO MAYBE I can always disable the button if intervalRunning is true instead of showing the message
+	// TODO make the user also include how long the break should be default 5 mins
+	// TODO fix the spam pause when the time is 0 issue
 	//There are 1500 seconds in 25 minutes
 	
+	timeOptions = [1500,1500,1500,1500]
+	restOptions = [900,900,900]
+	FREQUENCY = 10
 	state = {
-		time: 25,
-		paused: false,
+		time: this.timeOptions[0],
+		paused: true,
+		onBreak: false,
 		intervalRunning: false,
 		timesRan: 0,
 		message: '',
+		option:'current',
 	}
-	intervalSet = (frequency) => {
-		console.log('thisis being set')
-		this.timer = setInterval(this.tick, frequency)
+	intervalSet = (tickerFunction,frequency) => {
+		if(!this.state.intervalRunning){
+			this.timer = setInterval(tickerFunction, frequency)
+			this.setState({intervalRunning: true})
+		}
 	}
 	startTimer = () =>{
-		// The timer should go down 
-		if(!this.state.intervalRunning){
-			this.setState({intervalRunning: true})
-			this.intervalSet(500)
-		}else{
+		//Shows that the user that you can only press the starte button once
+		if(this.state.intervalRunning){
+			var message = this.state.message
 			this.setState({message: 'You can only press the start once'})
+			setTimeout( ()=>this.setState( {message} ),1000 )
+			return
 		}
+		// The timer should go down 
+		this.setState({intervalRunning: true,message:'Start working', paused: false})
+		this.intervalSet(this.workTicker,this.FREQUENCY)
+
 	}
 	//Pause and resume timer SHOULD share the same button
 	//For now i'm going to make them completely seperated
 	//Toggle timer handles the pause and resume
 	toggleTimer = () =>{
+		if(this.state.time == 0){
+			return
+		}
+		//not Paused
 		if(!this.state.paused){
+			console.log('paused')
 
 			clearInterval(this.timer)
-			this.setState({paused: true, message:'Timer is PAUSED'})
-
-
+			this.setState({
+				paused: true,
+				message:'Timer is PAUSED',
+				intervalRunning: false,
+			})
+		//Paused
 		}else if(this.state.paused){
-
-			this.intervalSet(500)
-			this.setState({paused: false, message:''})
+			console.log('resumed')
+			if(this.state.onBreak){//Update the rest ticker
+				this.intervalSet(this.restTicker, this.FREQUENCY)
+				this.setState({paused: false, message:'',intervalRunning: true})
+			}else if(!this.state.onBreak){//Update the work ticker
+				this.intervalSet(this.workTicker,this.FREQUENCY)
+				this.setState({paused: false, message:'',intervalRunning: true})
+			}
 
 		}
 	}
 	// Resets the timer to its current value
 	resetTimer = () => {
 
-		clearInterval(this.timer)
-		this.setState({
-			time: 25,
-			intervalRunning: false,
-			message: '',
-		})
-
+		//Reset the current timer
+		if(this.state.option == 'current'){
+			clearInterval(this.timer)
+			this.setState({
+				time: this.timeOptions[this.state.timesRan],
+				intervalRunning: false,
+				message: '',
+			})
+		//Reset the full timer
+		}else if(this.state.option == 'full'){
+			try{
+				clearInterval(this.timer)
+				this.setState({
+					intervalRunning: false,
+					option: 'current',
+					timesRan: 0,
+					onBreak: false,
+					time: this.timeOptions[0],
+				})
+			}catch(err){
+				console.log('Error: ' + err)
+			}
+		}
 	}
 
 	//The function that is being repeated over and over again
 	//every {frequency} time
-	tick = () => {
-
-		this.setState({time: this.state.time - 1})
-
+	workTicker = () => {
 		//When the timer is done running, then the times ran will update
 		//Times ran will control which timer is going next
-		if(this.state.time <= 0){
-			this.setState({
-				message: 'The timer has run out',
-				timesRan: this.state.timesRan += 1,
-			})
-			clearInterval(this.timer)
+		try{
+			if(this.state.time <= 1){
+				//then we stop when the time runs out
+				//TODO Show a message thet the pomodoro is finished and reset the timer
+				if(this.state.timesRan == this.timeOptions.length - 1){
+					clearInterval(this.timer)
+					this.setState({
+					message: 'You pomodoro has finished and the time has reset',
+					option:'full'
+					})
+					this.resetTimer()
+					return
+
+				}else{
+					this.setState({
+						onBreak: true,
+						intervalRunning: false,
+						time: this.restOptions[this.state.timesRan],
+					})
+					clearInterval(this.timer)
+					//TODO After a full times has gone by we should call restTicker
+					setTimeout(this.intervalSet,2000,this.restTicker, this.FREQUENCY)
+					return
+				}
+			}
+			this.setState({time: this.state.time - 1,message:'Start Working'})
+
+		}catch(err){
+			this.setState({message: 'Error: '+ err})
 		}
 
 	}	
+	restTicker = () => {
+		//Make sure thet time is ran before timesRan so that we can get the correct value
+
+		try{
+			if(this.state.time <= 1){
+
+				this.setState({
+					time: this.timeOptions[this.state.timesRan],
+					timesRan: this.state.timesRan += 1,
+					onBreak: false,
+					intervalRunning: false,
+				})
+
+				clearInterval(this.timer)
+			//call the workTicker when restTime has run out
+				setTimeout(this.intervalSet,2000,this.workTicker, this.FREQUENCY)
+				return
+
+			}
+			this.setState({time: this.state.time - 1,message:'Take a rest'})
+
+		}catch(err){
+
+			this.setState({
+				message: 'Error:' + err
+			})
+		}
+
+	}
 	render() {
 
 		const { time } = this.state
